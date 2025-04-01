2e1120d0ef7ba9b0cd42e4d2b5585f985bb2ed54
Znaka5 <90087802+Znaka5@users.noreply.github.com>
2025-04-01 18:28:40 +0000

    little fixes

--- react-project/react-project/project-exam-2025-main/app/src/Components/Error/Error.jsx
+++
@@ -1,7 +0,0 @@
-export default function Error({
-    error,
-    show,
-    type
-}) {
-    return (<div className={`error-overlay ${type}`}>Error: {error} <button onClick={show}>ok.</button></div>)
-}
